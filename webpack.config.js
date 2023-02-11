5beb6fc5506c3611624ef3dffc58ef1e994da070
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-02-10 22:57:46 -0800

    add new script for live server and add webpack live server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {                 // new line
+    contentBase: './dist'      // new line
+  },                           // new line
   devtool: 'eval-source-map',
   plugins: [
     new CleanWebpackPlugin({
